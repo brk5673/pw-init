b2165e6bd21dcb4dbfd18bb06ce9f2759b151ef9
brk5673 <97960640+brk5673@users.noreply.github.com>
2025-03-18 21:28:03 -0300

    stage 69

---
+++ pw-apitest-app/tests/newArticle.setup.ts
@@ -0,0 +1,22 @@
+import { expect, test as setup } from '@playwright/test';
+
+setup('create new article', async({ request }) => {
+  const articlePost = await request.post('https://conduit-api.bondaracademy.com/api/articles', {
+    // headers: {
+    //   'Authorization': `Token ${token}`
+    // },
+    data: {
+      'article': {
+        'title': 'Test title jfc',
+        'description': 'Test description2',
+        'body': 'Test body2',
+        'tagList': []
+      }
+    }
+  })
+  expect(articlePost.status()).toEqual(201)
+  const response = await articlePost.json();
+  const slugID = response.article.slug
+  process.env['SLUGID'] = slugID
+
+})
